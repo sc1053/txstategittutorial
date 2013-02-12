e11fd8f38e8eec4ea66413f373007ece8a5d013d
Chan Stephen T <sc1053@dh231m17.(none)>
2013-02-12 17:57:10 -0600

    changed hello world

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main() {
 
-  cout << "Hello World" << endl;
+  cout << "Hello World!" << endl;
+  cout << "OK.. I changed something!" << endl;
   return EXIT_SUCCESS;
 }
 
